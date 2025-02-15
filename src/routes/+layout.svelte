62f68d456d5b768e7d5013ef78a9b1c809da37ed
Simon Green <simonisaacgreen@icloud.com>
2025-02-15 18:31:26 +0000

    Add navigation loading indicator

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,7 +4,30 @@
 
 	let { children } = $props();
 	import Header from '$lib/components/Header.svelte';
+	import { getStores } from '$app/stores';
+	import { expoOut } from 'svelte/easing';
+	import { slide } from 'svelte/transition';
+
+	const { navigating } = getStores();
 </script>
 
+{#if $navigating?.from}
+	<div
+		class="navigation-loader"
+		in:slide={{ delay: 100, duration: 12000, axis: 'x', easing: expoOut }}
+	></div>
+{/if}
 <Header />
 <main>{@render children()}</main>
+
+<style lang="css">
+	.navigation-loader {
+		position: fixed;
+		top: 0;
+		right: 0;
+		left: 0;
+		height: 4px;
+		z-index: 50;
+		background-color: #ff9500;
+	}
+</style>
