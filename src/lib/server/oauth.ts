561098102e09b288f3e134220c1a99b3e867bf1a
Simon Green <simonisaacgreen@icloud.com>
2025-02-17 17:56:16 +0000

    Try to resolve issue accessing vercel url env var

--- src/lib/server/oauth.ts
+++ src/lib/server/oauth.ts
@@ -2,7 +2,7 @@ import { Google } from 'arctic';
 import { GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET } from '$env/static/private';
 import { dev } from '$app/environment';
 
-const baseUrl = dev ? 'http://localhost:5173' : `https://${process.env.VERCEL_URL}`;
+const baseUrl = dev ? 'http://localhost:5173' : `https://sveltekit-statsdoku-spike.vercel.app`;
 
 export const google = new Google(
 	GOOGLE_CLIENT_ID,
